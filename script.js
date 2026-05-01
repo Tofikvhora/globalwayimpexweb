1ad142765299a744e79e0056675cb5672d25e0b1
Tofikvhora <135727942+Tofikvhora@users.noreply.github.com>
2026-05-01 16:51:20 +0530

    Add files via upload
    web

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+  document.getElementById("navMenu").classList.toggle("active");
+}
+
+/* Scroll Animation */
+const faders = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+  faders.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
